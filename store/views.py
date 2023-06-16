78f6ae1fa57712048fc19912129dbb8da6056645
Martin010 <martin.serret47@gmail.com>
2023-06-16 18:49:26 +0200

    - Category in navbar - Category in store page - Product detail template and view - Get url for product - Links between home page, store page and product detail page - Product detail out of stock

--- store/views.py
+++ store/views.py
@@ -12,7 +12,6 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
-
     else:
         products = Product.objects.all().filter(is_available=True)
         product_count = products.count()
@@ -22,3 +21,15 @@ def store(request, category_slug=None):
         'product_count': product_count
     }
     return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug) # category__slug is the syntax to access to the slug
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
