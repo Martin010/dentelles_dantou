17b7179daeb36242360a0fa8ea38f1d41f81e652
Martin010 <martin.serret47@gmail.com>
2023-06-28 11:57:53 +0200

    - Registration preparation : urls and template - Registration : model forms and __init__ method

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Entrez un mot de passe',
+        'class': 'form-control',
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Répétez le mot de passe',
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Entrez votre prénom'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Entrez votre nom'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Entrez votre numéro de téléphone'
+        self.fields['email'].widget.attrs['placeholder'] = 'Entrez votre adresse email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
