e7ee23707f2257aa371c428d4abc2be6ce14067b
Martin010 <martin.serret47@gmail.com>
2023-07-26 10:08:47 +0200

    Product Gallery - requirements.txt: install pip package django-admin-thumbnails - store/models.py: creation of the model ProductGallery than contain a field image and a product foreign key - store/admin.py: creation of ProductGalleryInline to display the gallery in each product

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 
-from store.models import Product, Variation, ReviewRating
+from store.models import Product, Variation, ReviewRating, ProductGallery
+
+import admin_thumbnails
+
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    """ Allow to display product gallery in another model object"""
+    model = ProductGallery
+    extra = 1
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
     readonly_fields = ('created_date', 'modified_date')
+    inlines = [ProductGalleryInline]    # Display ProductGallery in the database product object
 
 
 class VariationAdmin(admin.ModelAdmin):
@@ -25,3 +35,4 @@ class ReviewRatingAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating, ReviewRatingAdmin)
+admin.site.register(ProductGallery)
