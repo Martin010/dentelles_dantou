eb036a0240bb266e89f55cc9ae1048490a48ff84
Martin010 <martin.serret47@gmail.com>
2023-07-26 11:55:45 +0200

    Product Gallery - store/views.py: get product gallery in product_detail function - templates/store/product_detail.html: display gallery images - templates/includes/footer.html: jquery script to display the correct image when a user click on its icon in the gallery - static/css/custom.css: gallery images style

--- store/views.py
+++ store/views.py
@@ -8,7 +8,7 @@ from carts.views import _cart_id
 from category.models import Category
 from orders.models import OrderProduct
 from store.forms import ReviewForm
-from store.models import Product, ReviewRating
+from store.models import Product, ReviewRating, ProductGallery
 
 
 def store(request, category_slug=None):
@@ -67,11 +67,15 @@ def product_detail(request, category_slug, product_slug):
     # Get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'order_product': order_product,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
